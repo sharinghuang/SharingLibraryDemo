f2f1c2fd77185af482f39a5f5a14da8caa553529
xueming.huang <huangsharing.evergrande.com>
2018-05-24 16:07:26 +0800

    aaaaa

--- app/src/main/java/com/sharing/demo/MainActivity.java
+++ app/src/main/java/com/sharing/demo/MainActivity.java
@@ -1,9 +1,10 @@
 package com.sharing.demo;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.sharing.library.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
